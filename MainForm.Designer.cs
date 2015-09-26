4a0b7acae6daa5a2d049dc0aa268e49b40441791
Martin Chuckeles <martin@jakubik.info>
2015-09-26 23:30:44 +0200

    Change the title

--- MainForm.Designer.cs
+++ MainForm.Designer.cs
@@ -23,9 +23,17 @@
     /// the contents of this method with the code editor.
     /// </summary>
     private void InitializeComponent() {
-      this.components = new System.ComponentModel.Container();
+      this.SuspendLayout();
+      // 
+      // MainForm
+      // 
+      this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
       this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-      this.Text = "MainForm";
+      this.ClientSize = new System.Drawing.Size(284, 261);
+      this.Name = "MainForm";
+      this.Text = "Udpit";
+      this.ResumeLayout(false);
+
     }
 
     #endregion
