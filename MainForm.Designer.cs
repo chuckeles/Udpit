05a0d920cd37c8149654af786d057f3158a764aa
Martin Chuckeles <martin@jakubik.info>
2015-09-26 23:48:16 +0200

    Added a big message box

--- MainForm.Designer.cs
+++ MainForm.Designer.cs
@@ -23,20 +23,37 @@
     /// the contents of this method with the code editor.
     /// </summary>
     private void InitializeComponent() {
+      this.messageBox = new System.Windows.Forms.TextBox();
       this.SuspendLayout();
+      // 
+      // messageBox
+      // 
+      this.messageBox.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+      this.messageBox.Location = new System.Drawing.Point(12, 12);
+      this.messageBox.Multiline = true;
+      this.messageBox.Name = "messageBox";
+      this.messageBox.ReadOnly = true;
+      this.messageBox.Size = new System.Drawing.Size(260, 199);
+      this.messageBox.TabIndex = 0;
       // 
       // MainForm
       // 
       this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
       this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
       this.ClientSize = new System.Drawing.Size(284, 261);
+      this.Controls.Add(this.messageBox);
       this.MinimumSize = new System.Drawing.Size(300, 300);
       this.Name = "MainForm";
       this.Text = "Udpit";
       this.ResumeLayout(false);
+      this.PerformLayout();
 
     }
 
     #endregion
+
+    private System.Windows.Forms.TextBox messageBox;
   }
 }
