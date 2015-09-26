a6e9b539716ece6b10f7ed4777147f7bac8d4f6a
Martin Chuckeles <martin@jakubik.info>
2015-09-27 00:48:49 +0200

    Added destination label

--- OptionsForm.Designer.cs
+++ OptionsForm.Designer.cs
@@ -23,13 +23,24 @@
     /// the contents of this method with the code editor.
     /// </summary>
     private void InitializeComponent() {
+      this.destinationLabel = new System.Windows.Forms.Label();
       this.SuspendLayout();
+      // 
+      // destinationLabel
+      // 
+      this.destinationLabel.AutoSize = true;
+      this.destinationLabel.Location = new System.Drawing.Point(12, 9);
+      this.destinationLabel.Name = "destinationLabel";
+      this.destinationLabel.Size = new System.Drawing.Size(58, 13);
+      this.destinationLabel.TabIndex = 0;
+      this.destinationLabel.Text = "destination";
       // 
       // OptionsForm
       // 
       this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
       this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
       this.ClientSize = new System.Drawing.Size(284, 161);
+      this.Controls.Add(this.destinationLabel);
       this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedSingle;
       this.MaximizeBox = false;
       this.MinimizeBox = false;
@@ -37,9 +48,12 @@
       this.StartPosition = System.Windows.Forms.FormStartPosition.CenterParent;
       this.Text = "options";
       this.ResumeLayout(false);
+      this.PerformLayout();
 
     }
 
     #endregion
+
+    private System.Windows.Forms.Label destinationLabel;
   }
 }
