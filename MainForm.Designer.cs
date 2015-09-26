bc3c7da03a8b9ee2be84b6409335797afdf9b103
Martin Chuckeles <martin@jakubik.info>
2015-09-26 23:38:01 +0200

    Added settingsGroupBox

--- MainForm.Designer.cs
+++ MainForm.Designer.cs
@@ -23,13 +23,26 @@
     /// the contents of this method with the code editor.
     /// </summary>
     private void InitializeComponent() {
+      this.settingsGroupBox = new System.Windows.Forms.GroupBox();
       this.SuspendLayout();
+      // 
+      // settingsGroupBox
+      // 
+      this.settingsGroupBox.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+      this.settingsGroupBox.Location = new System.Drawing.Point(13, 12);
+      this.settingsGroupBox.Name = "settingsGroupBox";
+      this.settingsGroupBox.Size = new System.Drawing.Size(259, 101);
+      this.settingsGroupBox.TabIndex = 0;
+      this.settingsGroupBox.TabStop = false;
+      this.settingsGroupBox.Text = "Settings";
       // 
       // MainForm
       // 
       this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
       this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
       this.ClientSize = new System.Drawing.Size(284, 261);
+      this.Controls.Add(this.settingsGroupBox);
       this.Name = "MainForm";
       this.Text = "Udpit";
       this.ResumeLayout(false);
@@ -37,5 +50,7 @@
     }
 
     #endregion
+
+    private System.Windows.Forms.GroupBox settingsGroupBox;
   }
 }
