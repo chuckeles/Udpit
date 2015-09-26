f1c9aafa54373f4355710b5334d2812ef1111a0d
Martin Chuckeles <martin@jakubik.info>
2015-09-27 00:37:36 +0200

    Added about text

--- AboutForm.Designer.cs
+++ AboutForm.Designer.cs
@@ -23,20 +23,41 @@
     /// the contents of this method with the code editor.
     /// </summary>
     private void InitializeComponent() {
+      this.aboutBox = new System.Windows.Forms.RichTextBox();
       this.SuspendLayout();
+      // 
+      // aboutBox
+      // 
+      this.aboutBox.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+      this.aboutBox.BorderStyle = System.Windows.Forms.BorderStyle.None;
+      this.aboutBox.Location = new System.Drawing.Point(12, 12);
+      this.aboutBox.Name = "aboutBox";
+      this.aboutBox.ReadOnly = true;
+      this.aboutBox.Size = new System.Drawing.Size(160, 137);
+      this.aboutBox.TabIndex = 0;
+      this.aboutBox.TabStop = false;
+      this.aboutBox.Text = "Udpit is a simple chatting application communicating over the UDP protocol.\n\nSour" +
+    "ce code is on Github: https://github.com/chuckeles/Udpit.";
       // 
       // AboutForm
       // 
       this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
       this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
       this.ClientSize = new System.Drawing.Size(184, 161);
-      this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedToolWindow;
+      this.Controls.Add(this.aboutBox);
+      this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedSingle;
+      this.MaximizeBox = false;
+      this.MinimizeBox = false;
       this.Name = "AboutForm";
-      this.Text = "about udpit";
+      this.Text = "about";
       this.ResumeLayout(false);
 
     }
 
     #endregion
+
+    private System.Windows.Forms.RichTextBox aboutBox;
   }
 }
