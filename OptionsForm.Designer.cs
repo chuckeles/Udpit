c18865db6060895dbca48338f8ce5fb928b59c24
Martin Chuckeles <martin@jakubik.info>
2015-09-27 00:49:54 +0200

    Added destination box

--- OptionsForm.Designer.cs
+++ OptionsForm.Designer.cs
@@ -24,22 +24,31 @@
     /// </summary>
     private void InitializeComponent() {
       this.destinationLabel = new System.Windows.Forms.Label();
+      this.destinationBox = new System.Windows.Forms.MaskedTextBox();
       this.SuspendLayout();
       // 
       // destinationLabel
       // 
       this.destinationLabel.AutoSize = true;
-      this.destinationLabel.Location = new System.Drawing.Point(12, 9);
+      this.destinationLabel.Location = new System.Drawing.Point(12, 15);
       this.destinationLabel.Name = "destinationLabel";
       this.destinationLabel.Size = new System.Drawing.Size(58, 13);
       this.destinationLabel.TabIndex = 0;
       this.destinationLabel.Text = "destination";
+      // 
+      // destinationBox
+      // 
+      this.destinationBox.Location = new System.Drawing.Point(76, 12);
+      this.destinationBox.Name = "destinationBox";
+      this.destinationBox.Size = new System.Drawing.Size(100, 20);
+      this.destinationBox.TabIndex = 1;
       // 
       // OptionsForm
       // 
       this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
       this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
       this.ClientSize = new System.Drawing.Size(284, 161);
+      this.Controls.Add(this.destinationBox);
       this.Controls.Add(this.destinationLabel);
       this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedSingle;
       this.MaximizeBox = false;
@@ -55,5 +64,6 @@
     #endregion
 
     private System.Windows.Forms.Label destinationLabel;
+    private System.Windows.Forms.MaskedTextBox destinationBox;
   }
 }
