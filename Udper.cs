afb7027f3df039228fc59fd789ffc467a7aa8f36
Martin Chuckeles <martin@jakubik.info>
2015-09-27 11:50:17 +0200

    Added Udper

---
+++ Udper.cs
@@ -0,0 +1,44 @@
+﻿namespace Udpit {
+
+  /// <summary>
+  ///   The UDP messaging system.
+  /// </summary>
+  internal class Udper {
+
+    public Udper() {
+      // check singleton
+      if (Singleton != null)
+        return;
+
+      // set singleton
+      Singleton = this;
+    }
+
+    /// <summary>
+    ///   The udper singleton.
+    /// </summary>
+    public Udper Singleton { get; }
+
+    /// <summary>
+    ///   The destination url.
+    /// </summary>
+    public string Destination;
+
+    /// <summary>
+    ///   Maximum fragment size.
+    /// </summary>
+    public int MaxFragment;
+
+    /// <summary>
+    ///   The user's name.
+    /// </summary>
+    public string Name;
+
+    /// <summary>
+    ///   Whether to send error fragments for testing purposes.
+    /// </summary>
+    public bool SendError;
+
+  }
+
+}
