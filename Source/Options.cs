f2c16653d8676467d1b15300e8fce5223afb9308
Martin Chuckeles <martin@jakubik.info>
2015-10-09 23:09:43 +0200

    Added Options

---
+++ Source/Options.cs
@@ -0,0 +1,15 @@
+﻿namespace Udpit {
+
+  /// <summary>
+  ///   Application options.
+  /// </summary>
+  internal static class Options {
+
+    /// <summary>
+    ///   User's name.
+    /// </summary>
+    public static string Name { get; set; }
+
+  }
+
+}
