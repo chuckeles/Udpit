d49e7fc46ca6e71fb778d12f4be78f84d574802f
Martin Chuckeles <martin@jakubik.info>
2015-10-09 21:57:38 +0200

    Added MessageCenter

---
+++ Source/MessageCenter.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections.Generic;
+
+namespace Udpit {
+
+  /// <summary>
+  ///   Message hub. Contains all messages in progress.
+  /// </summary>
+  internal class MessageCenter {
+
+    /// <summary>
+    ///   The list of messages in progress.
+    /// </summary>
+    private List<Message> _messages;
+
+  }
+
+}
