10b0c08560cc7f20cbab480a9538d92dd759db32
Martin Chuckeles <martin@jakubik.info>
2015-10-10 12:21:29 +0200

    Added save and cancel buttons

--- Source/NameForm.Designer.cs
+++ Source/NameForm.Designer.cs
@@ -23,13 +23,38 @@
     /// the contents of this method with the code editor.
     /// </summary>
     private void InitializeComponent() {
+      this.cancelButton = new System.Windows.Forms.Button();
+      this.saveButton = new System.Windows.Forms.Button();
       this.SuspendLayout();
+      // 
+      // cancelButton
+      // 
+      this.cancelButton.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Right)));
+      this.cancelButton.DialogResult = System.Windows.Forms.DialogResult.Cancel;
+      this.cancelButton.Location = new System.Drawing.Point(297, 223);
+      this.cancelButton.Name = "cancelButton";
+      this.cancelButton.Size = new System.Drawing.Size(75, 26);
+      this.cancelButton.TabIndex = 1;
+      this.cancelButton.Text = "Cancel";
+      this.cancelButton.UseVisualStyleBackColor = true;
+      // 
+      // saveButton
+      // 
+      this.saveButton.DialogResult = System.Windows.Forms.DialogResult.OK;
+      this.saveButton.Location = new System.Drawing.Point(216, 223);
+      this.saveButton.Name = "saveButton";
+      this.saveButton.Size = new System.Drawing.Size(75, 26);
+      this.saveButton.TabIndex = 1;
+      this.saveButton.Text = "Save";
+      this.saveButton.UseVisualStyleBackColor = true;
       // 
       // NameForm
       // 
       this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
       this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
       this.ClientSize = new System.Drawing.Size(384, 261);
+      this.Controls.Add(this.saveButton);
+      this.Controls.Add(this.cancelButton);
       this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedDialog;
       this.MaximizeBox = false;
       this.MinimizeBox = false;
@@ -41,5 +66,8 @@
     }
 
     #endregion
+
+    private System.Windows.Forms.Button cancelButton;
+    private System.Windows.Forms.Button saveButton;
   }
 }
