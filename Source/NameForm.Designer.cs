dd20bb2359f681e769520e4da90230cfc1d8af9e
Martin Chuckeles <martin@jakubik.info>
2015-10-10 12:28:14 +0200

    Added name box and label

--- Source/NameForm.Designer.cs
+++ Source/NameForm.Designer.cs
@@ -25,13 +25,15 @@
     private void InitializeComponent() {
       this.cancelButton = new System.Windows.Forms.Button();
       this.saveButton = new System.Windows.Forms.Button();
+      this.nameBox = new System.Windows.Forms.TextBox();
+      this.nameLabel = new System.Windows.Forms.Label();
       this.SuspendLayout();
       // 
       // cancelButton
       // 
       this.cancelButton.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Right)));
       this.cancelButton.DialogResult = System.Windows.Forms.DialogResult.Cancel;
-      this.cancelButton.Location = new System.Drawing.Point(297, 223);
+      this.cancelButton.Location = new System.Drawing.Point(197, 123);
       this.cancelButton.Name = "cancelButton";
       this.cancelButton.Size = new System.Drawing.Size(75, 26);
       this.cancelButton.TabIndex = 1;
@@ -40,19 +42,40 @@
       // 
       // saveButton
       // 
+      this.saveButton.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Right)));
       this.saveButton.DialogResult = System.Windows.Forms.DialogResult.OK;
-      this.saveButton.Location = new System.Drawing.Point(216, 223);
+      this.saveButton.Location = new System.Drawing.Point(116, 123);
       this.saveButton.Name = "saveButton";
       this.saveButton.Size = new System.Drawing.Size(75, 26);
-      this.saveButton.TabIndex = 1;
+      this.saveButton.TabIndex = 0;
       this.saveButton.Text = "Save";
       this.saveButton.UseVisualStyleBackColor = true;
+      // 
+      // nameBox
+      // 
+      this.nameBox.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+      this.nameBox.Location = new System.Drawing.Point(53, 12);
+      this.nameBox.Name = "nameBox";
+      this.nameBox.Size = new System.Drawing.Size(219, 20);
+      this.nameBox.TabIndex = 2;
+      // 
+      // nameLabel
+      // 
+      this.nameLabel.AutoSize = true;
+      this.nameLabel.Location = new System.Drawing.Point(12, 15);
+      this.nameLabel.Name = "nameLabel";
+      this.nameLabel.Size = new System.Drawing.Size(35, 13);
+      this.nameLabel.TabIndex = 3;
+      this.nameLabel.Text = "Name";
       // 
       // NameForm
       // 
       this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
       this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-      this.ClientSize = new System.Drawing.Size(384, 261);
+      this.ClientSize = new System.Drawing.Size(284, 161);
+      this.Controls.Add(this.nameLabel);
+      this.Controls.Add(this.nameBox);
       this.Controls.Add(this.saveButton);
       this.Controls.Add(this.cancelButton);
       this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedDialog;
@@ -62,6 +85,7 @@
       this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
       this.Text = "Name";
       this.ResumeLayout(false);
+      this.PerformLayout();
 
     }
 
@@ -69,5 +93,7 @@
 
     private System.Windows.Forms.Button cancelButton;
     private System.Windows.Forms.Button saveButton;
+    private System.Windows.Forms.TextBox nameBox;
+    private System.Windows.Forms.Label nameLabel;
   }
 }
