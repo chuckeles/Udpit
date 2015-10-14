92265fa1334e81722c5e11c38f6754d54541fe70
Martin Chuckeles <martin@jakubik.info>
2015-10-14 14:03:44 +0200

    Added options for ports

--- Source/Options.cs
+++ Source/Options.cs
@@ -10,6 +10,16 @@
     /// </summary>
     public static string Name { get; set; } = "Newbie";
 
+    /// <summary>
+    ///   Port on which to listen for data.
+    /// </summary>
+    public static int ReceivePort { get; set; } = 10695;
+
+    /// <summary>
+    ///   Port on which to send data.
+    /// </summary>
+    public static int SendPort { get; set; } = 10694;
+
   }
 
 }
