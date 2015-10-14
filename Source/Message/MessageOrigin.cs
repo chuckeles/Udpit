ea45217aef5a0145897165d05a938df73b184b00
Martin Chuckeles <martin@jakubik.info>
2015-10-14 12:16:17 +0200

    Moved Message Added MessageOrigin

---
+++ Source/Message/MessageOrigin.cs
@@ -0,0 +1,20 @@
+﻿namespace Udpit {
+
+  /// <summary>
+  ///   Enumeration of message origin types.
+  /// </summary>
+  internal enum MessageOrigin {
+
+    /// <summary>
+    /// The message was created locally.
+    /// </summary>
+    Local,
+
+    /// <summary>
+    /// The message was received from a remote.
+    /// </summary>
+    Remote
+
+  }
+
+}
