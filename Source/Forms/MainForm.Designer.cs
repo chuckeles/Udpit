eeb6a6e80086149f2a0c4510ec4350a2b4c4ece1
Martin Chuckeles <martin@jakubik.info>
2015-10-26 18:17:05 +0100

    Filled the send tab

--- Source/Forms/MainForm.Designer.cs
+++ Source/Forms/MainForm.Designer.cs
@@ -33,8 +33,30 @@
       this._tabSend = new System.Windows.Forms.TabPage();
       this._tabReceive = new System.Windows.Forms.TabPage();
       this._tabOptions = new System.Windows.Forms.TabPage();
+      this._logBox = new System.Windows.Forms.TextBox();
+      this._sendTableContainer = new System.Windows.Forms.TableLayoutPanel();
+      this._sendInputGroup = new System.Windows.Forms.GroupBox();
+      this._sendFileGroup = new System.Windows.Forms.GroupBox();
+      this._sendActionsGroup = new System.Windows.Forms.GroupBox();
+      this._sendInputBox = new System.Windows.Forms.TextBox();
+      this._sendFileContainer = new System.Windows.Forms.TableLayoutPanel();
+      this._sendFileButton = new System.Windows.Forms.Button();
+      this._sendFileRemoveButton = new System.Windows.Forms.Button();
+      this._sendFileLabel = new System.Windows.Forms.Label();
+      this._sendFileName = new System.Windows.Forms.Label();
+      this._sendActionsContainer = new System.Windows.Forms.TableLayoutPanel();
+      this._sendButton = new System.Windows.Forms.Button();
+      this._sendCancelButton = new System.Windows.Forms.Button();
       this._mainMenu.SuspendLayout();
       this._tabContainer.SuspendLayout();
+      this._tabLog.SuspendLayout();
+      this._tabSend.SuspendLayout();
+      this._sendTableContainer.SuspendLayout();
+      this._sendInputGroup.SuspendLayout();
+      this._sendFileGroup.SuspendLayout();
+      this._sendActionsGroup.SuspendLayout();
+      this._sendFileContainer.SuspendLayout();
+      this._sendActionsContainer.SuspendLayout();
       this.SuspendLayout();
       // 
       // _mainMenu
@@ -96,6 +118,7 @@
       // 
       // _tabLog
       // 
+      this._tabLog.Controls.Add(this._logBox);
       this._tabLog.Location = new System.Drawing.Point(4, 22);
       this._tabLog.Name = "_tabLog";
       this._tabLog.Padding = new System.Windows.Forms.Padding(3);
@@ -106,6 +129,7 @@
       // 
       // _tabSend
       // 
+      this._tabSend.Controls.Add(this._sendTableContainer);
       this._tabSend.Location = new System.Drawing.Point(4, 22);
       this._tabSend.Name = "_tabSend";
       this._tabSend.Padding = new System.Windows.Forms.Padding(3);
@@ -132,6 +156,174 @@
       this._tabOptions.Text = "Options";
       this._tabOptions.UseVisualStyleBackColor = true;
       // 
+      // _logBox
+      // 
+      this._logBox.Dock = System.Windows.Forms.DockStyle.Fill;
+      this._logBox.Location = new System.Drawing.Point(3, 3);
+      this._logBox.Multiline = true;
+      this._logBox.Name = "_logBox";
+      this._logBox.ReadOnly = true;
+      this._logBox.Size = new System.Drawing.Size(270, 383);
+      this._logBox.TabIndex = 0;
+      // 
+      // _sendTableContainer
+      // 
+      this._sendTableContainer.ColumnCount = 1;
+      this._sendTableContainer.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle());
+      this._sendTableContainer.Controls.Add(this._sendInputGroup, 0, 0);
+      this._sendTableContainer.Controls.Add(this._sendFileGroup, 0, 1);
+      this._sendTableContainer.Controls.Add(this._sendActionsGroup, 0, 2);
+      this._sendTableContainer.Dock = System.Windows.Forms.DockStyle.Fill;
+      this._sendTableContainer.Location = new System.Drawing.Point(3, 3);
+      this._sendTableContainer.Name = "_sendTableContainer";
+      this._sendTableContainer.RowCount = 3;
+      this._sendTableContainer.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 40F));
+      this._sendTableContainer.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 40F));
+      this._sendTableContainer.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 20F));
+      this._sendTableContainer.Size = new System.Drawing.Size(270, 383);
+      this._sendTableContainer.TabIndex = 0;
+      // 
+      // _sendInputGroup
+      // 
+      this._sendInputGroup.Controls.Add(this._sendInputBox);
+      this._sendInputGroup.Dock = System.Windows.Forms.DockStyle.Fill;
+      this._sendInputGroup.Location = new System.Drawing.Point(3, 3);
+      this._sendInputGroup.Name = "_sendInputGroup";
+      this._sendInputGroup.Size = new System.Drawing.Size(264, 147);
+      this._sendInputGroup.TabIndex = 0;
+      this._sendInputGroup.TabStop = false;
+      this._sendInputGroup.Text = "Input Text";
+      // 
+      // _sendFileGroup
+      // 
+      this._sendFileGroup.Controls.Add(this._sendFileContainer);
+      this._sendFileGroup.Dock = System.Windows.Forms.DockStyle.Fill;
+      this._sendFileGroup.Location = new System.Drawing.Point(3, 156);
+      this._sendFileGroup.Name = "_sendFileGroup";
+      this._sendFileGroup.Size = new System.Drawing.Size(264, 147);
+      this._sendFileGroup.TabIndex = 1;
+      this._sendFileGroup.TabStop = false;
+      this._sendFileGroup.Text = "Input File";
+      // 
+      // _sendActionsGroup
+      // 
+      this._sendActionsGroup.Controls.Add(this._sendActionsContainer);
+      this._sendActionsGroup.Dock = System.Windows.Forms.DockStyle.Fill;
+      this._sendActionsGroup.Location = new System.Drawing.Point(3, 309);
+      this._sendActionsGroup.Name = "_sendActionsGroup";
+      this._sendActionsGroup.Size = new System.Drawing.Size(264, 71);
+      this._sendActionsGroup.TabIndex = 2;
+      this._sendActionsGroup.TabStop = false;
+      this._sendActionsGroup.Text = "Actions";
+      // 
+      // _sendInputBox
+      // 
+      this._sendInputBox.Dock = System.Windows.Forms.DockStyle.Fill;
+      this._sendInputBox.Location = new System.Drawing.Point(3, 16);
+      this._sendInputBox.Multiline = true;
+      this._sendInputBox.Name = "_sendInputBox";
+      this._sendInputBox.Size = new System.Drawing.Size(258, 128);
+      this._sendInputBox.TabIndex = 0;
+      // 
+      // _sendFileContainer
+      // 
+      this._sendFileContainer.ColumnCount = 2;
+      this._sendFileContainer.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 50F));
+      this._sendFileContainer.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 50F));
+      this._sendFileContainer.Controls.Add(this._sendFileButton, 0, 1);
+      this._sendFileContainer.Controls.Add(this._sendFileRemoveButton, 1, 1);
+      this._sendFileContainer.Controls.Add(this._sendFileLabel, 0, 0);
+      this._sendFileContainer.Controls.Add(this._sendFileName, 1, 0);
+      this._sendFileContainer.Dock = System.Windows.Forms.DockStyle.Fill;
+      this._sendFileContainer.Location = new System.Drawing.Point(3, 16);
+      this._sendFileContainer.Name = "_sendFileContainer";
+      this._sendFileContainer.RowCount = 2;
+      this._sendFileContainer.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 50F));
+      this._sendFileContainer.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 50F));
+      this._sendFileContainer.Size = new System.Drawing.Size(258, 128);
+      this._sendFileContainer.TabIndex = 0;
+      // 
+      // _sendFileButton
+      // 
+      this._sendFileButton.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+      this._sendFileButton.Location = new System.Drawing.Point(3, 102);
+      this._sendFileButton.Name = "_sendFileButton";
+      this._sendFileButton.Size = new System.Drawing.Size(123, 23);
+      this._sendFileButton.TabIndex = 2;
+      this._sendFileButton.Text = "Select";
+      this._sendFileButton.UseVisualStyleBackColor = true;
+      // 
+      // _sendFileRemoveButton
+      // 
+      this._sendFileRemoveButton.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+      this._sendFileRemoveButton.Location = new System.Drawing.Point(132, 102);
+      this._sendFileRemoveButton.Name = "_sendFileRemoveButton";
+      this._sendFileRemoveButton.Size = new System.Drawing.Size(123, 23);
+      this._sendFileRemoveButton.TabIndex = 3;
+      this._sendFileRemoveButton.Text = "Remove";
+      this._sendFileRemoveButton.UseVisualStyleBackColor = true;
+      // 
+      // _sendFileLabel
+      // 
+      this._sendFileLabel.AutoSize = true;
+      this._sendFileLabel.Dock = System.Windows.Forms.DockStyle.Fill;
+      this._sendFileLabel.Location = new System.Drawing.Point(3, 0);
+      this._sendFileLabel.Name = "_sendFileLabel";
+      this._sendFileLabel.Size = new System.Drawing.Size(123, 64);
+      this._sendFileLabel.TabIndex = 0;
+      this._sendFileLabel.Text = "Selected file:";
+      this._sendFileLabel.TextAlign = System.Drawing.ContentAlignment.MiddleRight;
+      // 
+      // _sendFileName
+      // 
+      this._sendFileName.AutoSize = true;
+      this._sendFileName.Dock = System.Windows.Forms.DockStyle.Fill;
+      this._sendFileName.Location = new System.Drawing.Point(132, 0);
+      this._sendFileName.Name = "_sendFileName";
+      this._sendFileName.Size = new System.Drawing.Size(123, 64);
+      this._sendFileName.TabIndex = 1;
+      this._sendFileName.Text = "file.txt";
+      this._sendFileName.TextAlign = System.Drawing.ContentAlignment.MiddleLeft;
+      // 
+      // _sendActionsContainer
+      // 
+      this._sendActionsContainer.ColumnCount = 2;
+      this._sendActionsContainer.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 50F));
+      this._sendActionsContainer.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 50F));
+      this._sendActionsContainer.Controls.Add(this._sendButton, 0, 0);
+      this._sendActionsContainer.Controls.Add(this._sendCancelButton, 1, 0);
+      this._sendActionsContainer.Dock = System.Windows.Forms.DockStyle.Fill;
+      this._sendActionsContainer.Location = new System.Drawing.Point(3, 16);
+      this._sendActionsContainer.Name = "_sendActionsContainer";
+      this._sendActionsContainer.RowCount = 1;
+      this._sendActionsContainer.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 50F));
+      this._sendActionsContainer.Size = new System.Drawing.Size(258, 52);
+      this._sendActionsContainer.TabIndex = 0;
+      // 
+      // _sendButton
+      // 
+      this._sendButton.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+      this._sendButton.Location = new System.Drawing.Point(3, 26);
+      this._sendButton.Name = "_sendButton";
+      this._sendButton.Size = new System.Drawing.Size(123, 23);
+      this._sendButton.TabIndex = 0;
+      this._sendButton.Text = "Send";
+      this._sendButton.UseVisualStyleBackColor = true;
+      // 
+      // _sendCancelButton
+      // 
+      this._sendCancelButton.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+      this._sendCancelButton.Location = new System.Drawing.Point(132, 26);
+      this._sendCancelButton.Name = "_sendCancelButton";
+      this._sendCancelButton.Size = new System.Drawing.Size(123, 23);
+      this._sendCancelButton.TabIndex = 1;
+      this._sendCancelButton.Text = "Cancel";
+      this._sendCancelButton.UseVisualStyleBackColor = true;
+      // 
       // MainForm
       // 
       this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
@@ -149,6 +341,17 @@
       this._mainMenu.ResumeLayout(false);
       this._mainMenu.PerformLayout();
       this._tabContainer.ResumeLayout(false);
+      this._tabLog.ResumeLayout(false);
+      this._tabLog.PerformLayout();
+      this._tabSend.ResumeLayout(false);
+      this._sendTableContainer.ResumeLayout(false);
+      this._sendInputGroup.ResumeLayout(false);
+      this._sendInputGroup.PerformLayout();
+      this._sendFileGroup.ResumeLayout(false);
+      this._sendActionsGroup.ResumeLayout(false);
+      this._sendFileContainer.ResumeLayout(false);
+      this._sendFileContainer.PerformLayout();
+      this._sendActionsContainer.ResumeLayout(false);
       this.ResumeLayout(false);
       this.PerformLayout();
 
@@ -166,5 +369,19 @@
     private System.Windows.Forms.TabPage _tabSend;
     private System.Windows.Forms.TabPage _tabReceive;
     private System.Windows.Forms.TabPage _tabOptions;
+    private System.Windows.Forms.TextBox _logBox;
+    private System.Windows.Forms.TableLayoutPanel _sendTableContainer;
+    private System.Windows.Forms.GroupBox _sendInputGroup;
+    private System.Windows.Forms.GroupBox _sendFileGroup;
+    private System.Windows.Forms.GroupBox _sendActionsGroup;
+    private System.Windows.Forms.TextBox _sendInputBox;
+    private System.Windows.Forms.TableLayoutPanel _sendFileContainer;
+    private System.Windows.Forms.Button _sendFileButton;
+    private System.Windows.Forms.Button _sendFileRemoveButton;
+    private System.Windows.Forms.Label _sendFileLabel;
+    private System.Windows.Forms.Label _sendFileName;
+    private System.Windows.Forms.TableLayoutPanel _sendActionsContainer;
+    private System.Windows.Forms.Button _sendButton;
+    private System.Windows.Forms.Button _sendCancelButton;
   }
 }
