ca6e6453c59089449e53cdc744642e1eeddc1b0d
Martin Chuckeles <martin@jakubik.info>
2015-10-26 17:45:58 +0100

    Added MainForm

---
+++ Source/Forms/MainForm.cs
@@ -0,0 +1,16 @@
+﻿using System.Windows.Forms;
+
+namespace Udpit {
+
+  /// <summary>
+  ///   The main application form.
+  /// </summary>
+  public partial class MainForm : Form {
+
+    public MainForm() {
+      InitializeComponent();
+    }
+
+  }
+
+}
