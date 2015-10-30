be5c5fa8def672f7a967af47de8a5b0086bb3958
Martin Chuckeles <martin@jakubik.info>
2015-10-30 20:59:31 +0100

    Added placeholder methods for the CRC

---
+++ Source/Fragment/CRC.cs
@@ -0,0 +1,33 @@
+﻿using System.Linq;
+
+namespace Udpit {
+
+  /// <summary>
+  ///   The CRC thing to check and generate checksums for fragments.
+  /// </summary>
+  public static class CRC {
+
+    /// <summary>
+    ///   Generates a checksum and appends it to a fragment.
+    /// </summary>
+    public static void GenerateChecksum(ref byte[] fragment) {
+      // get checksum
+      var checksum = GetChecksum(fragment);
+
+      // append it
+      fragment = fragment.Concat(checksum).ToArray();
+    }
+
+    /// <summary>
+    ///   Checks a fragment and removes the checksum.
+    /// </summary>
+    public static bool CheckFragment(ref byte[] fragment) {}
+
+    /// <summary>
+    ///   Generates a checksum.
+    /// </summary>
+    private static byte[] GetChecksum(byte[] fragment) {}
+
+  }
+
+}
