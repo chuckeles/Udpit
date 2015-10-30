e8234f37f925f43f2bd2e153a8066b9f28e72c29
Martin Chuckeles <martin@jakubik.info>
2015-10-30 21:17:45 +0100

    Copied some CRC algorithm, need my own

--- Source/Fragment/CRC.cs
+++ Source/Fragment/CRC.cs
@@ -1,4 +1,5 @@
-﻿using System.Linq;
+﻿using System;
+using System.Linq;
 
 namespace Udpit {
 
@@ -15,18 +16,50 @@ namespace Udpit {
       var checksum = GetChecksum(fragment);
 
       // append it
-      fragment = fragment.Concat(checksum).ToArray();
+      fragment = fragment.Concat(BitConverter.GetBytes(checksum)).ToArray();
     }
 
     /// <summary>
     ///   Checks a fragment and removes the checksum.
     /// </summary>
-    public static bool CheckFragment(ref byte[] fragment) {}
+    public static bool CheckFragment(ref byte[] fragment) {
+      // remove last 2 bytes, that's the checksum 
+      var checksum = BitConverter.ToUInt16(fragment.Reverse().Take(2).Reverse().ToArray(), 0);
+      fragment = fragment.Reverse().Skip(2).Reverse().ToArray();
+
+      // generate checksum and compare
+      return (checksum == GetChecksum(fragment));
+    }
 
     /// <summary>
     ///   Generates a checksum.
     /// </summary>
-    private static byte[] GetChecksum(byte[] fragment) {}
+    private static ushort GetChecksum(byte[] fragment) {
+      // TODO: Figure this out and write your own
+      ushort Polynominal = 0x1021;
+      ushort InitValue = 0x0;
+
+      ushort i, j, index = 0;
+      ushort CRC = InitValue;
+      ushort Remainder, tmp, short_c;
+      for (i = 0; i < fragment.Length; i++) {
+        short_c = (ushort)(0x00ff & (ushort)fragment[index]);
+        tmp = (ushort)((CRC >> 8) ^ short_c);
+        Remainder = (ushort)(tmp << 8);
+        for (j = 0; j < 8; j++) {
+
+          if ((Remainder & 0x8000) != 0) {
+            Remainder = (ushort)((Remainder << 1) ^ Polynominal);
+          }
+          else {
+            Remainder = (ushort)(Remainder << 1);
+          }
+        }
+        CRC = (ushort)((CRC << 8) ^ Remainder);
+        index++;
+      }
+      return CRC;
+    }
 
   }
 
